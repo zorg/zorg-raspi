97af53a688e1f340ff672d189aad9807ee0f181a
Kevin Brown <kevin@kevinbrown.in>
2015-07-15 20:22:01 -0400

    Set up PWM support
    Right now we are defining the arguments in terms of the frequency
    and the duty cycle. On the Edison this is technically the period
    and duty cycle, but the Raspberry Pi doesn't actually match the
    frequency properly anyway.
    
    These two parameters will need to be standardized in the near
    future across Zorg adaptors, so the argument names can line up
    and requests to the API can function properly.

--- zorg_raspi/adaptor.py
+++ zorg_raspi/adaptor.py
@@ -9,7 +9,21 @@ class RasPi(Adaptor):
         
         GPIO.setmode(GPIO.BOARD)
         
+        self.pwm = {}
+        
     def digital_write(self, pin_number, value):
         GPIO.setup(pin_number, GPIO.OUT)
         
         GPIO.output(pin_number, value)
+        
+    def pwm_write(self, pin_number, duty_cycle, frequency):
+        if pin_number not in self.pwm:
+            GPIO.setup(pin_number, GPIO.OUT)
+        
+            self.pwm[pin_number] = GPIO.PWM(pin_number, frequency)
+            self.pwm[pin_number].start(duty_cycle)
+            
+        pin = self.pwm[pin_number]
+        
+        pin.ChangeFrequency(frequency)
+        pin.ChangeDutyCycle(duty_cycle)
